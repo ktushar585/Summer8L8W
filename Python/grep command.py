b490e2b8364fb719e5c28126b973312b4119f20d
ktushar585 <64252275+ktushar585@users.noreply.github.com>
2020-05-03 16:53:23 +0530

    Update grep command.py

--- Python/grep command.py
+++ Python/grep command.py
@@ -8,5 +8,7 @@ args=parser.parse_args()
 with args.file2 as file1:
     mylist=file1.readlines()
 for item in mylist:
-    if 'word' in item:
-        print(item)
+    mylist2=item.split()
+    for initem in mylist2:
+        if args.word == initem:
+            print(item)
